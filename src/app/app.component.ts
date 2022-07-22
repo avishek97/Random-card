ce13c70c327db95879725a085b885f2841971f6f
Avishek <johndoe@email.com>
2022-07-22 22:47:49 +0530

    Template & Props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from './services/user.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'random-cards';
+  users:any;
+  constructor(private user:UserService,private toastr: ToastrService){}
+  ngOnInit(): void {
+    this.user.getUser().subscribe(
+      (user:any)=>{
+        console.log(user);
+        
+        this.users=user.results[0]
+      },
+      (error)=>{
+        this.toastr.error("OOps! ")
+      }
+    )
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// http
+import {HttpClientModule} from '@angular/common/http'
+// toastr
+
+
 import { AppComponent } from './app.component';
+import {} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ToastrModule } from 'ngx-toastr'
+import {MatIconModule} from '@angular/material/icon';
+import { CardComponent } from './card/card.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
